0745315be7ec190ff0f82fa6cfe631499f335fac
Eliasld <area_causation872@simplelogin.fr>
2025-07-10 20:50:07 +0200

    Optimize temporary images directory deletion

--- cmd/main.go
+++ cmd/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
 
 	"github.com/EliasLd/scan-scraper/internal/fetch"
 	"github.com/EliasLd/scan-scraper/internal/convert"
@@ -113,21 +114,22 @@ func main() {
 			fmt.Printf("Filtered to %d chapters from range %d-%d.\n", len(chapters), opts.Range[0], opts.Range[1])
 		}
 
+		homeDir, err := os.UserHomeDir()
+		if err != nil {
+			log.Fatalf("Failed to get user home directory: %v", err)
+		}
+
 		for _, chapter := range chapters {
 			fmt.Printf("Downloading chapter %s...\n", chapter)
-			
-			homeDir, err := os.UserHomeDir()
-			if err != nil {
-				log.Fatalf("Failed to get user home directory: %v", err)
-			}
-			imageDir := fmt.Sprintf("%s/Images/%s/%s", homeDir, opts.Slug, chapter)
+
+			imageDir := filepath.Join(homeDir, "Images", opts.Slug, chapter) 
 			err = fetch.DownloadChapter(opts.Slug, chapter, imageDir)
 			if err != nil {
 				log.Printf("Failed to download chapter %s: %v", chapter, err)
 				continue
 			}
 
-			pdfPath := fmt.Sprintf("%s/%s_%s.pdf", opts.ScanDir, opts.Slug, chapter)
+			pdfPath := filepath.Join(opts.ScanDir, fmt.Sprintf("%s_%s.pdf", opts.Slug, chapter))
 			err = convert.ImagesToPDF(imageDir, pdfPath, opts.Cleanup)
 			if err != nil {
 				log.Printf("Failed to create PDF for chapter %s: %v\n", chapter, err)
@@ -136,6 +138,16 @@ func main() {
 
 			fmt.Printf("Chapter %s downloaded and saved as %s\n", chapter, pdfPath)
 		}
+
+		if opts.Cleanup {
+			rootImagesDir := filepath.Join(homeDir, "Images", opts.Slug)
+			fmt.Printf("Cleaning up images directory: %s\n", rootImagesDir)
+			err := os.RemoveAll(rootImagesDir)
+			if err != nil {
+				log.Fatalf("Failed to remove dir: %w", err)
+			}
+		}
+
 	} else {
 		fmt.Println("Please use --all or --range to download chapters.")
 	}
