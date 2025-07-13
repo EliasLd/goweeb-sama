c693b2bd80209a48c1a36b3e1ecd9fc1845ba3ef
Eliasld <area_causation872@simplelogin.fr>
2025-07-13 12:35:34 +0200

    Add InitialModel tui function

--- internal/tui/model.go
+++ internal/tui/model.go
@@ -15,4 +15,29 @@ type Model struct {
 	Cursor		int
 }
 
+func InitialModel() Model {
+	manga := textinput.New()
+	manga.Placeholder = "Entrer le nom du manga (espace = -)"
+	manga.Focus()
+	manga.Prompt = "> "
+	manga.CharLimit = 50
 
+	rangeInput := textinput.New()
+	rangeInput.Placeholder = "exemple: 10-77"
+	rangeInput.Prompt("> ")
+
+	scanDir := textinput.New()
+	scanDir.Placeholder = "Dossier de destination"
+	scanDir.Prompt = "> "
+	scanDir.SetValue("pdf")
+
+	return Model {
+		Title:		"Scan scraper",
+		MangaInput:	manga,
+		AllCheckBox:	checkbox.New("Télécharger tous les chapitres", false),
+		RangeInput:	rangeInput,
+		ScanDirInput:	scanDir,
+		KeepCheckbox:	checkbox.New("Garder les images après conversion", false),
+		Cursor:		0,
+	}
+}
