4477831a33268eb342aec82e832d9a8d1dac19c4
Eliasld <area_causation872@simplelogin.fr>
2025-07-14 11:22:58 +0200

    Update tui model structure

--- internal/tui/model.go
+++ internal/tui/model.go
@@ -1,43 +1,58 @@
 package tui
 
 import (
-	"github.com/charmbracelet/bubbles/checkbox"
 	"github.com/charmbracelet/bubbles/textinput"
+	tea "github.com/charmbracelet/bubbletea"
 )
 
 type Model struct {
 	Title		string
 	MangaInput	textinput.Model
-	AllCheckbox	checkbox.Model
+	AllCheckbox	Checkbox
 	RangeInput	textinput.Model
 	ScanDirInput	textinput.Model
-	KeepCheckbox	checkbox.Model
+	KeepCheckbox	Checkbox
 	Cursor		int
 }
 
 func InitialModel() Model {
 	manga := textinput.New()
-	manga.Placeholder = "Entrer le nom du manga (espace = -)"
+	manga.Placeholder = "ex: jujutsu-kaisen"
 	manga.Focus()
 	manga.Prompt = "> "
 	manga.CharLimit = 50
+	manga.Width = 60
 
 	rangeInput := textinput.New()
-	rangeInput.Placeholder = "exemple: 10-77"
-	rangeInput.Prompt("> ")
+	rangeInput.Placeholder = "ex: 1-28"
+	rangeInput.Prompt = "> "
+	rangeInput.Width = 60
 
 	scanDir := textinput.New()
-	scanDir.Placeholder = "Dossier de destination"
+	scanDir.Placeholder = "ex: C:\\Users\\<username>\\Documents\\scans\\jjk"
 	scanDir.Prompt = "> "
-	scanDir.SetValue("pdf")
+	scanDir.Width = 70
 
 	return Model {
-		Title:		"Scan scraper",
-		MangaInput:	manga,
-		AllCheckBox:	checkbox.New("Télécharger tous les chapitres", false),
-		RangeInput:	rangeInput,
-		ScanDirInput:	scanDir,
-		KeepCheckbox:	checkbox.New("Garder les images après conversion", false),
-		Cursor:		0,
+		Title:       "Scan scraper",
+		MangaInput:  manga,
+		AllCheckbox: Checkbox{Label: "Télécharger tous les chapitres.", Checked: false},
+		RangeInput:  rangeInput,
+		ScanDirInput: scanDir,
+		KeepCheckbox: Checkbox{Label: "Garder les images après conversion (déconseillé).", Checked: false},
+		Cursor:      0,
 	}
 }
+
+func (m Model) Init() tea.Cmd {
+	return nil 
+}
+
+func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	return Update(msg, m)
+}
+
+func (m Model) View() string {
+    	return View(m)
+}
+
