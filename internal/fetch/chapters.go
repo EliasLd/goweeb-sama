7f003519bf242ef95cb2b8aa2f3d2e75fa9d42c0
EliasLd <area_causation872@simplelogin.fr>
2026-02-07 14:52:02 +0100

    Update chapter searching utility to handle new range format

--- internal/fetch/chapters.go
+++ internal/fetch/chapters.go
@@ -9,8 +9,10 @@ import (
 )
 
 // Deduces the number of chapters
-// using the access url pattern
-func GetChapters(slug string, writer io.Writer) ([]string, error) {
+// using the access url pattern.
+// If endCheck is 0, it will search until
+// maxErrorsInARow consecutive failures
+func GetChapters(slug string, startCheck, endCheck int, writer io.Writer) ([]string, error) {
 	correctName, err := DetectCorrectMangaName(slug, 1, writer)
 	if err != nil {
 		return nil, err
@@ -23,7 +25,7 @@ func GetChapters(slug string, writer io.Writer) ([]string, error) {
 	const maxRetries = 3
 
 	errorsCounter := 0
-	i := 0
+	i := startCheck
 
 	// Reusable http client with timeout
 	client := &http.Client{
@@ -31,6 +33,13 @@ func GetChapters(slug string, writer io.Writer) ([]string, error) {
 	}
 
 	for {
+
+		// If endCheck is specified and we've passed it, stop
+		if endCheck > 0 && i > endCheck {
+			fmt.Fprintln(writer, "[L] Reached enf of specified range")
+			break
+		}
+
 		url := fmt.Sprintf("%s/%d/1.jpg", baseURL, i)
 		fmt.Fprintln(writer, "Checking:", url)
 
@@ -65,7 +74,9 @@ func GetChapters(slug string, writer io.Writer) ([]string, error) {
 		} else {
 			fmt.Fprintln(writer, "Did not find url: ", url)
 			errorsCounter++
-			if errorsCounter >= maxErrorsInARow {
+
+			// Only stop on consecutive errors if no endCheck is specified
+			if endCheck == 0 && errorsCounter >= maxErrorsInARow {
 				// We're assuming there's no more chapters to fetch
 				fmt.Fprintln(writer, "Reached max request failures, resuming...")
 				break
